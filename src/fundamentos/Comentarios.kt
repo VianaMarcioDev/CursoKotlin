ef8abb2be656231bae139b0183f9604e9f8f5636
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-08-21 15:41:18 -0300

    learning types of commentary

---
+++ src/fundamentos/Comentarios.kt
@@ -0,0 +1,19 @@
+package fundamentos
+
+/**
+ * comentário tipo k-doc
+ */
+
+
+//Comentário de uma linha
+
+
+/*
+    Comentário de
+    mais de uma linha
+*/
+
+
+fun main(args: Array<String>) {
+
+}
